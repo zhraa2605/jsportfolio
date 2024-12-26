ab4ae55deb128181a06d59b07186472840cb95d4
zhraa2605 <zhraa.e.l.2605@gmail.com>
2024-12-26 08:00:28 +0300

    sceond commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex just" >
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 " >
+      <div className="max-w-7xl w-full">
+        </div>
 
 
     </main>
