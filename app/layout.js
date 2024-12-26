7d02334cb089c5121ef493546f38c66424b0b9be
zhraa2605 <zhraa.e.l.2605@gmail.com>
2024-12-26 14:44:04 +0300

    second commit

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,24 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Portfolio",
-  description: "Modern and Minimalist Portfolio",
-};
+import { ThemeProvider } from "./theme-provider.tsx"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
-
+import Hero from "../components/Hero.tsx";
+import {FloatingNav} from "../components/ui/FloatingNavbar.tsx"
+import { FaHome } from "react-icons/fa";
+import Grid from "../components/Grid.tsx";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 " >
-      <div className="max-w-7xl w-full">
+      <div className="max-w-7xl w-full ">
+        <FloatingNav navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]} />
+        <Hero />
+        <Grid />
+        
+
+
         </div>
 
 
